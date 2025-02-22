986cb68b4dced26880a4679b5dc71fe2ac351c55
fishm995 <mark88590@gmail.com>
2025-02-22 16:04:25 -0600

    Update app.py

--- app.py
+++ app.py
@@ -162,4 +162,4 @@ def settings_page():
     return render_template('settings.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=False)
