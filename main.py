cdef6c46d280c383c45417b6fd9747b1f6dfed6e
fishm995 <mark88590@gmail.com>
2025-03-05 20:02:41 -0600

    Update main.py

--- main.py
+++ main.py
@@ -240,4 +240,4 @@ if __name__ == "__main__":
     atexit.register(lambda: scheduler.shutdown())
     
     # Run the Flask web application on host 0.0.0.0 (accessible externally) and port 5000.
-    #app.run(host='0.0.0.0', port=5000, debug=False)
+    app.run(host='0.0.0.0', port=5000, debug=False)
