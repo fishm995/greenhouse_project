4768424a713a3729360ba12d6bfa51fcdf6dd3d8
fishm995 <mark88590@gmail.com>
2025-04-27 15:06:51 -0500

    Update app.py

--- app.py
+++ app.py
@@ -125,8 +125,7 @@ def public_status():
                 value = sensor_instance.read_value()
                 print(f"[Public Status] Sensor '{sensor.sensor_name}' reading: {value}", flush=True)
                 if isinstance(value, dict):
-                  sensor_readings[f"{sensor.sensor_name}_Temp"]  = value["temperature"]
-                  sensor_readings[f"{sensor.sensor_name}_Humid"] = value["humidity"]
+                  sensor_readings[f"{sensor.sensor_name}]  = value["temperature"]
                 else:
                   sensor_readings[sensor.sensor_name] = value
             except Exception as e:
